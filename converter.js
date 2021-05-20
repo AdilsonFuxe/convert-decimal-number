1b906de9f9f4cb6499578d02cbfbddb256321bf6
Lucas Almeida <developer.lucas24@gmail.com>
2021-05-20 22:50:39 +0100

    validation (#2)
    * chore: add english language json
    
    * chore: add english language json
    
    * feat: add validation and support to english language
    
    * test: add validation and support to english language

--- converter.js
+++ converter.js
@@ -1,33 +1,63 @@
-const ptNumbers = require('./pt-numbers.json');
+const ptNumbers = require('./lang/pt-numbers.json');
+const enNumbers = require('./lang/en-numbers.json');
 
-const convertToExt = (number = 0) => {
-  if (number >= 0 && number < 20) return ptNumbers[number];
-  if (number < 100) {
-    const resto = number % 10;
-    const numberMenosResto = number - resto;
-
-    if (resto === 0) return ptNumbers[numberMenosResto];
-    return ptNumbers[numberMenosResto] + ' e ' + convertToExt(resto);
-  }
-  if (number < 1000) {
-    const resto = number % 100;
-    const numberMenosResto = number - resto;
-
-    if (resto === 0) return ptNumbers[numberMenosResto];
-    return ptNumbers[numberMenosResto] + ' e ' + convertToExt(resto);
-  }
-  if (number < 1000000) {
-    const resto = number % 1000;
-    const result = Math.trunc(number / 1000);
-
-    if (resto === 0) return result === 1 ? 'MIL' : convertToExt(result);
-    return (
-      (result === 1 ? 'Mil' : convertToExt(result) + ' MIL') +
-      ' e ' +
-      convertToExt(resto)
-    );
-  }
-  if (number === 1000000) return ptNumbers[1000000];
+const language = {
+  portuguese: 'pt',
+  english: 'en',
 };
+
+const chooseLanguage = {
+  pt: ptNumbers,
+  en: enNumbers,
+};
+
+const convertToExt = (number = 0, lang = 'pt') => {
+  if (typeof Number(number) === 'number') {
+    let numberLang = {};
+
+    if (lang === 'pt') {
+      numberLang = chooseLanguage[language.portuguese];
+    } else if (lang === 'en') {
+      numberLang = chooseLanguage[language.english];
+    }
+
+    if (number >= 0 && number < 20) return numberLang[number];
+    if (number < 100) {
+      const rest = number % 10;
+      const numberMinusRest = number - rest;
+
+      return rest === 0
+        ? numberLang[numberMinusRest]
+        : `${numberLang[numberMinusRest]} e ${convertToExt(rest)}`;
+    }
+
+    if (number < 1000) {
+      const rest = number % 100;
+      const numberMinusRest = number - rest;
+      return rest === 0
+        ? numberLang[numberMinusRest]
+        : `${numberLang[numberMinusRest]} e ${convertToExt(rest)}`;
+    }
+
+    if (number < 1000000) {
+      const rest = number % 1000;
+      const result = Math.trunc(number / 1000);
+
+      if (rest === 0) return result === 1 ? 'Mil' : convertToExt(result);
+      return (
+        (result === 1 ? 'Mil' : convertToExt(result) + ' MIL') +
+        ' e ' +
+        convertToExt(rest)
+      );
+    }
+    if (number === 1000000) return ptNumbers[1000000];
+  }
+
+  return lang === chooseLanguage[language.portuguese]
+    ? `${number} não é um número.`
+    : `${number} is not a number.`;
+};
+
+console.log(convertToExt(1000, 'pt'));
 
 module.exports = convertToExt;
