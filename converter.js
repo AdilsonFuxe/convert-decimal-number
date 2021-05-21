99abe1663d11014859906a575bf32704ccfcaacf
Lucas Almeida <developer.lucas24@gmail.com>
2021-05-21 20:15:25 +0100

    refactor: simplify convertToLiteral function

--- converter.js
+++ converter.js
@@ -1,57 +1,54 @@
 const ptNumbers = require('./lang/pt-numbers.json');
 const enNumbers = require('./lang/en-numbers.json');
 
-const language = {
-  portuguese: 'pt',
-  english: 'en',
-};
-
 const chooseLanguage = {
   pt: ptNumbers,
   en: enNumbers,
 };
 
-const convertToExt = (number = 0, lang) => {
-  if (typeof Number(number) === 'number') {
-    let numberLang = chooseLanguage[lang];
+const NO_SOLUTION = '--';
 
-    if (number >= 0 && number < 20) return numberLang[number];
-    if (number < 100) {
-      const rest = number % 10;
-      const numberMinusRest = number - rest;
+const setNumber = (number) => {
+  let num = 0;
 
-      return rest === 0
-        ? numberLang[numberMinusRest]
-        : `${numberLang[numberMinusRest]} e ${convertToExt(rest)}`;
-    }
-
-    if (number < 1000) {
-      const rest = number % 100;
-      const numberMinusRest = number - rest;
-      return rest === 0
-        ? numberLang[numberMinusRest]
-        : `${numberLang[numberMinusRest]} e ${convertToExt(rest)}`;
-    }
-
-    if (number < 1000000) {
-      const rest = number % 1000;
-      const result = Math.trunc(number / 1000);
-
-      if (rest === 0) return result === 1 ? 'Mil' : convertToExt(result);
-      return (
-        (result === 1 ? 'Mil' : convertToExt(result) + ' MIL') +
-        ' e ' +
-        convertToExt(rest)
-      );
-    }
-    if (number === 1000000) return ptNumbers[1000000];
+  if (number < 100) {
+    num = 10;
+  } else if (number < 1000) {
+    num = 100;
+  } else if (number < 10000) {
+    num = 1000;
+  } else if (number < 100000) {
+    num = 10000;
+  } else if (number < 1000000) {
+    num = 100000;
+  } else if (number < 10000000) {
+    num = 1000000;
+  } else if (number < 100000000) {
+    num = 10000000;
   }
 
-  return lang === chooseLanguage[language.portuguese]
-    ? `${number} não é um número.`
-    : `${number} is not a number.`;
+  return num;
 };
 
-console.log(convertToExt(100, 'pt'));
+const convertToLiteral = (number, lang) => {
+  let divider;
+  let res = 0;
 
-module.exports = convertToExt;
+  if (lang !== 'pt' && lang !== 'en')
+    throw new Error('This language is not supported yet');
+  if (typeof number !== 'number') throw new Error('You must provide a number');
+
+  const words = chooseLanguage[lang];
+
+  if (words[number]) {
+    return words[number];
+  }
+
+  divider = setNumber(number);
+  res = parseInt(number / divider) * divider;
+  return `${words[res]} and ${convertToLiteral(number % divider, lang)}`;
+};
+
+console.log(convertToLiteral(1111111, 'en'));
+
+module.exports = convertToLiteral;
